90473c8dc8deb91d4ee0b2302fe5331d27c3815e
MohamedGoush <0x1110111@gmail.com>
2023-10-29 06:42:58 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by python on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
